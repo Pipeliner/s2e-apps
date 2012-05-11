f2efcaed0f7e59568f6ad5d40e1760c405947e29
Vadim Evard <v.e.evard@gmail.com>
2012-05-12 03:31:57 +0400

    Libcfree demo. No weird return address mismatches like those in programs linked with libc.

---
+++ demos/libcfree/overflower.c
@@ -0,0 +1,38 @@
+#include "s2e.h"
+int i;
+void f1() {
+	i++;
+	i++;
+	i++;
+	//s2e_message("Hi from f1()\n");
+	i--;
+
+}
+int main(int argc, char **argv) {
+	__asm__ __volatile__(
+		"mov %%ecx, %0"
+		: "=r"(argc)
+	);
+
+	//s2e_message("main() start\n");
+	i++;
+	i++;
+	i++;
+	i++;
+	i++;
+	i++;
+	if (argc > 1) {
+		i++;
+		i++;
+		f1();
+	}
+	i++;
+	i++;
+	i++;
+	i++;
+	i++;
+	i++;
+	i++;
+	//s2e_message("main() end\n");
+}
+
